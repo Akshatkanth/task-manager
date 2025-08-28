838ec23c61ac580c780ac45dbd146494909b3546
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-08-28 19:38:46 +0530

    express server setup

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+
+
+//routes          //(req, res) is a call back function
+app.get('/hello', (req, res)=>{
+    res.send('task manager app!')
+})
+
+const port = 3000 //hardcoding rn
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
