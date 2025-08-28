0937e6c481410320f90f0464321b2deef44474c5
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-08-28 19:53:24 +0530

    routes

--- starter/app.js
+++ starter/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-
+const routes = require('./routes/tasks')
 
 
 //routes          //(req, res) is a call back function
 app.get('/hello', (req, res)=>{
     res.send('task manager app!')
 })
+
+app.use('/api/v1/tasks', tasks)
 
 const port = 3000 //hardcoding rn
 
