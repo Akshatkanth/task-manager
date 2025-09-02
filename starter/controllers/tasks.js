c2a9b01018a74be3f8e17a5e8503709452fc915d
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-02 19:36:33 +0530

    error handling

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -1,11 +1,19 @@
 const Task = require('../models/Task')
 const getAllTasks = (req, res) =>{
+    try{
+        const tasks = await Task.find({})
+    }
     res.send('get all tasks')
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
+    try{
+        const task = await Task.create(req.body)
+        res.status(201).json({task})
+    }catch(error){
+        res.status(500).json({msg: error})
+    }
+    
 }
 
 const getTask = (req, res) => {
