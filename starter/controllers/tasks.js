20296b4075435c5978ffd02224566292155ecfb3
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-04 20:34:28 +0530

    getalltask api

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -2,7 +2,9 @@ const Task = require('../models/Task')
 const getAllTasks = async(req, res) =>{
     try{
         const tasks = await Task.find({})
-        res.status(200).json({tasks})
+        // res.status(200).json({tasks})
+        // res.status(200).json({tasks, amount:tasks.length}) 
+        res.status(200).json({status:"success", data:{tasks, nbHits:tasks.length}})
     }catch(error){
         res.status(500).json({msg: error})
     }
@@ -68,24 +70,7 @@ const updateTask = async(req, res) => {
     }
 }
 
-const editTask = async(req, res) => {
-    try {
-        const {id:taskID} = req.params
 
-        const task = await Task.findOneAndUpdate({_id:taskID}, req.body, {
-            new: true,
-            runValidators: true,
-        })
-
-        if(!task){
-            return res.status(404).json({msg: `No task with id : ${taskID}`})
-        }
-
-        res.status(200).json({task}) 
-    } catch (error) {
-        
-    }
-}
 
 
 module.exports = {
@@ -94,5 +79,4 @@ module.exports = {
     getTask, 
     updateTask, 
     deleteTask,
-    editTask,
 }
