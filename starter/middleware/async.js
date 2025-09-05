7ee53d6962a64297d63ec7b96aa47b3f2639c063
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2025-09-05 19:58:42 +0530

    async setup

--- starter/middleware/async.js
+++ starter/middleware/async.js
@@ -1,5 +1,11 @@
-const asyncWrapper = () =>{
-
+const asyncWrapper = (fn) =>{
+    return async(req, res, next) => {
+        try {
+            awaitfn(req, res, next)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 
